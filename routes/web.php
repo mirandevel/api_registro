fa8e2d6920f6733e00c392242170353e9eb67f8f
mirandevel <miranda.devel@gmail.com>
2021-01-05 19:59:04 -0400

    tarea

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('storage/{filename}', function ($filename)
+{
+    $url=URL::asset('storage/profile/'.$filename) ;
+    return '<img src="'.$url.'"/>';
+   /* $path = storage_path('profile/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;*/
+});
